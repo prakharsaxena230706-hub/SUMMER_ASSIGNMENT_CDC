25e79522d2a8c2487c304dbdc2917304b6f67f2f
[prakharsaxena230706-hub] <prakharsaxena230706@gmail.com>
2026-06-08 13:50:41 +0530

    Maximum_Average_Subarray.c

--- WEEK_01/Day_02/Maximum_Average_Subarray.c
+++ WEEK_01/Day_02/Maximum_Average_Subarray.c
@@ -1,37 +1,37 @@
 #include <stdio.h>
 
 double findMaxAverage(int* nums, int numsSize, int k) {
-    long long sum = 0;
-    for (int i = 0; i < k; i++) {
-        sum += nums[i];
+    long long sum=0;
+    for (int i=0; i<k; i++) {
+        sum+=nums[i];
     }
     
-    long long maxSum = sum;
-    int startIndex = 0;
-    int endIndex = k;
+    long long maxSum=sum;
+    int startIndex=0;
+    int endIndex=k;
     
-    while (endIndex < numsSize) {
-        sum -= nums[startIndex];
+    while(endIndex<numsSize) {
+        sum-=nums[startIndex];
         startIndex++;
         
-        sum += nums[endIndex];
+        sum+=nums[endIndex];
         endIndex++;
         
-        if (sum > maxSum) {
-            maxSum = sum;
+        if(sum>maxSum) {
+            maxSum=sum;
         }
     }
-    return (double)maxSum / k;
+    return (double)maxSum/k;
 }
 
-int main() {
+int main(){
     
-    int nums[] = {1, 12, -5, -6, 50, 3};
-    int numsSize = sizeof(nums) / sizeof(nums[0]); 
-    int k = 4;
+    int nums[]={1,12,-5,-6,50,3};
+    int numsSize=sizeof(nums)/sizeof(nums[0]); 
+    int k=4;
 
-    double result = findMaxAverage(nums, numsSize, k);
-    printf("Maximum Average Subarray: %.5f\n", result);
+    double result=findMaxAverage(nums,numsSize,k);
+    printf("Maximum Average Subarray: %.5f\n",result);
 
     return 0;
 }
